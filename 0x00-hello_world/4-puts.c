c5f7bf9889500299a87cf93a7ac5ba71c73fa22f
kiruku14 <kiruku14@gmail.com>
2022-06-06 06:56:27 -0700

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,19 @@
+/**
+ * File: 4-puts.c
+ *Auth: Brian Kiruku
+ */
+
 #include <stdio.h>
 
+/**
+ * main - prints "Programming is like building a multilingual
+ * 		  puzzle, followed by a new line."
+ *
+ *Return: Always 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0)
+	return(0);
 }
