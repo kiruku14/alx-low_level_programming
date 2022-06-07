2fcd26ce1b1bedbff1d78e5013828cb2c5d17e47
kiruku14 <kiruku14@gmail.com>
2022-06-06 19:59:23 -0700

    l

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+	putchar(48 + c);
+	c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
