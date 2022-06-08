295cd9e068e654d5293c169ac5126ee8d0525571
kiruku14 <kiruku14@gmail.com>
2022-06-08 14:45:05 -0700

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ *
+ * return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int b = 0;
+
+	while (b < 10)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+
+		_putchar('\n');
+		b++;
+	}
+}
