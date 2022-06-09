f32324324c3f5a8ccf493c5175bcd37920ed425a
kiruku14 <kiruku14@gmail.com>
2022-06-09 12:47:24 -0700

    first bugs

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * positive_or_negative - checks code
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
