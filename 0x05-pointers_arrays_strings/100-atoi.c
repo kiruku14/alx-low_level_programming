b81b9b510458900c8bf7aa1c9d3cf7f25cfa5e4c
kiruku14 <kiruku14@gmail.com>
2022-06-14 19:20:21 -0700

    j

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _atoi - turns string to integers
+ * @s: variable
+ *
+ * Return: null
+ */
+
+int _atoi(char *s)
+{
+	int sign_cache = 1;
+	int result = 0;
+
+	while (s[0] != '\0')
+	{
+		if (s[0] == '-')
+			sign_cache *= -1;
+		else if (s[0] >= '0' && s[0] <= '9')
+			result = (result * 10) + (s[0] - '0') * sign_cache;
+		else if (result)
+			break;
+
+		s++;
+	}
+
+	return (result);
+}
