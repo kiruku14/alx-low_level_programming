af7d7a73cf3c1b099d24decabee446ccd072ec6c
kiruku14 <kiruku14@gmail.com>
2022-06-16 17:50:04 -0700

    solved

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,22 +2,22 @@
 
 /**
  * string_toupper - change lowercase to upper
- * @string: parameter
+ * @str: parameter
  *
  * Return: null
  */
 
-char string_toupper(char *string)
+char *string_toupper(char *str)
 {
 	int i = 0;
 
-	for (i = 0; string[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (string[i] >= 97 && string[i] <= 122)
+		if (str[i] >= 97 && str[i] <= 122)
 		{
-			string[i] = string[i] - 32;
+			str[i] = str[i] - 32;
 		}
 	}
-	
-	return (string);
+
+	return (str);
 }
